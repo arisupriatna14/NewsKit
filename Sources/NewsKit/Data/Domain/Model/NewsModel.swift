75d50cf894a6570208f52a0441e6e32507942984
Ari Supriatna <ari.supriatna@bibit.id>
2021-10-15 00:10:37 +0700

    refactor: add initializer for NewsModel and add ID for NewsModel

--- Sources/NewsKit/Data/Domain/Model/NewsModel.swift
+++ Sources/NewsKit/Data/Domain/Model/NewsModel.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-public struct NewsModel: Equatable {
+public struct NewsModel: Equatable, Identifiable {
+  /// The id of the article
+  public let id = UUID()
   /// The author of the article
   public let author: String
   /// The headline or title of the article
@@ -22,10 +24,48 @@ public struct NewsModel: Equatable {
   public let content: String
   /// The display name ``SourceModel/name`` for the source this article came from
   public let source: SourceModel
+  
+  public init(
+    author: String,
+    title: String,
+    description: String,
+    url: String,
+    urlToImage: String,
+    content: String,
+    source: SourceModel
+  ) {
+    self.author = author
+    self.title = title
+    self.description = description
+    self.url = url
+    self.urlToImage = urlToImage
+    self.content = content
+    self.source = source
+  }
 }
 
 /// The Source Response
 public struct SourceModel: Equatable {
   /// The name of source news
   public let name: String
+  
+  public init(name: String) {
+    self.name = name
+  }
+}
+
+extension NewsModel {
+  
+  public static var stub: NewsModel {
+    .init(
+      author: "ABC Australia",
+      title: "Vaksin COVID-19 yang Mulai Disuntikkan Pekan Ini, Adakah Efek Sampingnya? - detikNews",
+      description: "Pekan ini, Inggris akan melaksanakan vaksinasi COVID-19 kepada warganya. Namun, masih banyak pertanyaan seputar vaksin yang hingga kini belum terjawab.",
+      url: "https://news.detik.com/abc-australia/d-5286211/vaksin-covid-19-yang-mulai-disuntikkan-pekan-ini-adakah-efek-sampingnya",
+      urlToImage: "https://awsimages.detik.net.id/api/wm/2020/12/08/vaksin-covid-19-yang-mulai-disuntikkan-pekan-ini-adakah-efek-sampingnya_169.jpeg?wid=54&w=650&v=1&t=jpeg",
+      content: "Canberra - Pekan ini Inggris dijadwalkan akan melaksanakan vaksinasi COVID-19 kepada warganya. Namun, banyak pertanyaan seputar vaksin yang hingga kini belum terjawab.\r\nSementara di Australia sendiri… [+5777 chars]",
+      source: SourceModel(name: "Detik.com")
+    )
+  }
+  
 }
